8c1a94f7947f804a26a33927c6af499825acec8f
Vishal Kumar <122553234+VshalRawat@users.noreply.github.com>
2025-08-05 17:42:06 +0530

    Update q3_create_folder.py

--- q3_create_folder.py
+++ q3_create_folder.py
@@ -1,14 +1,12 @@
 import os
 
-# Folder name to create
+
 folder_name = "test_folder"
 
-# Full path
 path = os.path.join(os.getcwd(), folder_name)
 
-# Check if folder exists
 if not os.path.exists(path):
     os.mkdir(path)
-    print(f"📁 Folder '{folder_name}' created!")
+    print(f" Folder '{folder_name}' created!")
 else:
-    print(f"✅ Folder '{folder_name}' already exists.")
+    print(f"Folder '{folder_name}' already exists.")
